4f3a383e7eff983d8519ed66f17c9791dedd917c
weiyu <jcab1688@gamil.com>
2020-05-22 01:20:25 +0800

    add login form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { InputGroup, InputGroupAddon, Button, Jumbotron, Container,Row } from 'reactstrap';
+import { FontAwesomeIcon } from '../../node_modules/@fortawesome/react-fontawesome'
+import { faAngleDown, faAngleUp} from '../../node_modules/@fortawesome/free-solid-svg-icons'
+
+import './LoginForm.css'
+
+export default class LoginForm extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+    render (){
+        return(
+        	<div className="box ">
+				<p className='title'>News Sharing</p>
+				<p className='login'>Login Form</p>
+
+				<form role="form" method="post" className=''>
+					<div className="inputBox">
+						<input type="text" name="username" autocomplete="off" required/>
+						<label>Username</label>
+					</div>
+					<div className="inputBox">
+						<input type="password" name="password" autocomplete="off" required/>
+						<label>Password</label>
+					</div>
+					<div id='createAccount'>Create account</div>
+					<div id='createAccount'>Forget passward</div>
+					<input type="submit" name="login" value="Login"/>
+				</form>
+          	</div>
+
+
+
+        )
+    }
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,8 +19,8 @@ import './Main.css';
 import ArticleForm from 'components/ArticleForm.jsx';
 import Homepage from 'components/Homepage.jsx';
 import Manager from 'components/Manager.jsx';
-
 import CatalogPage from 'components/CatalogPage.jsx';
+import LoginForm from 'components/LoginForm.jsx';
 
 export default class Main extends React.Component {
     constructor(props) {
@@ -29,50 +29,54 @@ export default class Main extends React.Component {
 
     render() {
         return (
-            <Router>
-                <div className='main bg-faded'>
-                    <div className='container'>
-                        <Navbar color='faded' light expand='md'>
-                            <NavbarToggler/>
-                            <NavbarBrand className='text-info' href="/">NewsSharing</NavbarBrand>
-                                <Nav navbar>
-                                    <NavItem>
-                                        <NavLink tag={Link} to='/a'>Home</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink tag={Link} to='/food'>Food</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink tag={Link} to='/ArticleForm'>Make a Post(temporary)</NavLink> {/* testing only */}
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink tag={Link} to='/Manager'>Manager</NavLink> 
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink tag={Link} to='/'>Catalog</NavLink> 
-                                    </NavItem>
-                                </Nav>
-                        </Navbar>
+            // <Router>
+            //     <div className='main bg-faded'>
+            //         <div className='container'>
+            //             <Navbar color='faded' light expand='md'>
+            //                 <NavbarToggler/>
+            //                 <NavbarBrand className='text-info' href="/">NewsSharing</NavbarBrand>
+            //                     <Nav navbar>
+            //                         <NavItem>
+            //                             <NavLink tag={Link} to='/a'>Home</NavLink>
+            //                         </NavItem>
+            //                         <NavItem>
+            //                             <NavLink tag={Link} to='/food'>Food</NavLink>
+            //                         </NavItem>
+            //                         <NavItem>
+            //                             <NavLink tag={Link} to='/ArticleForm'>Make a Post(temporary)</NavLink> {/* testing only */}
+            //                         </NavItem>
+            //                         <NavItem>
+            //                             <NavLink tag={Link} to='/Manager'>Manager</NavLink> 
+            //                         </NavItem>
+            //                         <NavItem>
+            //                             <NavLink tag={Link} to='/'>Catalog</NavLink> 
+            //                         </NavItem>
+            //                     </Nav>
+            //             </Navbar>
                         
-                    </div>
+            //         </div>
 
-                    <Route exact path="/a" render={() => (
-                        <Homepage />
-                    )}/>
-                    <Route exact path="/food" render={() => (
-                        <h1>Food</h1>
-                    )}/>
-                    <Route exact path="/ArticleForm" render={() => (
-                        <ArticleForm/>  
-                    )}/>
-                    <Route exact path="/Manager" render={() => (
-                        <Manager/>  
-                    )}/>
-                    <Route exact path="/" render={() => (
-                        <CatalogPage topicName='food' description='Rishi Sunak has extended the government’s wage subsidy scheme until the end of October in a move that could see costs rise to more than £80 billion. The Treasury has decided not to cut the overall level of the wage subsidy scheme, which stands at 80 per cent of people’s wages up to £2,500 a month. It has instead announced that from August furloughed workers will'/>
-                    )}/>
-                </div>
-            </Router>
+            //         <Route exact path="/a" render={() => (
+            //             <Homepage />
+            //         )}/>
+            //         <Route exact path="/food" render={() => (
+            //             <h1>Food</h1>
+            //         )}/>
+            //         <Route exact path="/ArticleForm" render={() => (
+            //             <ArticleForm/>  
+            //         )}/>
+            //         <Route exact path="/Manager" render={() => (
+            //             <Manager/>  
+            //         )}/>
+            //         {/* <Route exact path="/" render={() => (
+            //             <CatalogPage topicName='food' description='Rishi Sunak has extended the government’s wage subsidy scheme until the end of October in a move that could see costs rise to more than £80 billion. The Treasury has decided not to cut the overall level of the wage subsidy scheme, which stands at 80 per cent of people’s wages up to £2,500 a month. It has instead announced that from August furloughed workers will'/>
+            //         )}/> */}
+            //         <Route exact path="/" render={() => (
+            //             <LoginForm/>
+            //         )}/>
+            //     </div>
+            // </Router>
+            <LoginForm/>
         );
     }
 }
