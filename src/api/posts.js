3007f329865ec50bd863eb6de983e4f9d781bc8c
Ryan-RE-Wang <44486000+Ryan-RE-Wang@users.noreply.github.com>
2020-05-23 20:34:56 +0800

    fail

---
+++ src/api/posts.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import { v4 as uuid } from 'uuid';
+import moment from 'moment';
+import '@babel/polyfill';
+
+const postKey = 'posts';
+
+export function listPosts(searchText = '') {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(_listPosts(searchText));
+        }, 500);
+    });
+}
+
+// Simulated server-side code
+function _listPosts(searchText = '') {
+    let postString = localStorage.getItem(postKey);
+    let posts = postString ? JSON.parse(postString) : [];
+    if (posts.length > 0 && searchText) {
+        posts = posts.filter(p => {
+            return p.text.toLocaleLowerCase().indexOf(searchText.toLowerCase()) !== -1
+        });
+    }
+    return posts;
+};
+
+export function createPost(club, title, content, date, time) {
+    return new Promise((resolve, reject) => {
+        resolve(_createPost(club, title, content, date, time));
+    });
+}
+
+// Simulated server-side code
+function _createPost(club, title, content, date, time) {
+    const newPost = {
+        id: uuid(),
+        club: club,
+        title: title,
+        content: content,
+        date: date,
+        time: time,
+        ts: moment().unix()
+    };
+    const posts = [
+        newPost,
+        ..._listPosts()
+    ];
+    localStorage.setItem(postKey, JSON.stringify(posts));
+    return newPost;
+}
+
+export function createVote(id, mood) {
+    return new Promise((resolve, reject) => {
+        _createVote(id, mood);
+        resolve();
+    });
+}
+
+// Simulated server-side code
+function _createVote(id, mood) {
+    const posts = _listPosts().map(p => {
+        if (p.id === id) {
+            p[mood.toLowerCase() + 'Votes']++;
+        }
+        return p;
+    });
+    localStorage.setItem(postKey, JSON.stringify(posts));
+}
