2fae1b44da0ff68f7d1367a78277c2a8a4b3b186
107062316 <lisa104433@gmail.com>
2020-05-31 13:36:50 +0800

    update ui and layout

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Row, Col, Label, Button } from 'reactstrap';
+import { Container, Row, Col, Label } from 'reactstrap';
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import PlaceIcon from '@material-ui/icons/Place';
 import PaymentIcon from '@material-ui/icons/Payment';
@@ -14,53 +16,73 @@ export default class Article extends React.Component {
     constructor(props) {
         super(props);
     }
+
     render() {
         return (
-            <Container className='article'>
-                
-                <h1 className='title'>This is an article</h1>
-
-                <Row xs="2" align="center"> 
-                    <Col xs="auto"> 
-                        <EventIcon/>
-                    </Col>
-                    <Col xs="auto"> 
-                        <p>Date</p>
-                    </Col>
-                </Row>
-                <Row xs="2" align="center"> 
-                    <Col xs="auto"> 
-                        <AccessTimeIcon/>
-                    </Col>
-                    <Col xs="auto"> 
-                        <p>Time</p>
-                    </Col>
-                </Row>
-                <Row xs="2" align="center"> 
-                    <Col xs="auto"> 
-                        <PlaceIcon/>
-                    </Col>
-                    <Col xs="auto"> 
-                        <p>Location</p>
-                    </Col>
-                </Row>
-                <Row xs="2" align="center"> 
-                    <Col xs="auto"> 
-                        <PaymentIcon/>
-                    </Col>
-                    <Col xs="auto"> 
-                        <p>Ticket info.</p>
-                    </Col>
-                </Row>
-                <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
-                </p>
-                <div className='poster-container'>
-                    <a href="#"><img class='poster' src="images/烤魷魚.jpg" alt="grilled squid" width="400rem" margin="0 auto"/></a>
+            <div className='article'>
+                <div className='title'>This is an article</div>
+                <div className='d-flex row content justify-content-center'>
+                    <div className='poster'>
+                        <a href="#"><img className='img-fluid' src="images/烤魷魚.jpg" alt="grilled squid" width="400rem" margin="0 auto"/></a>
+                    </div>
+                    <div className='info align-item-center'>
+                        <div className='d-flex row justify-content-start align-items-center'>
+                            <div className='p-2'> 
+                                <EventIcon/>
+                            </div> 
+                            <div className='p-2'>  
+                                Date &nbsp; 2020/06/22
+                            </div> 
+                            <div className=' p-2'>
+                                <Hook/> 
+                            </div>
+                        </div>
+                        <div className= 'd-flex row justify-content-start align-items-center'>
+                            <div className='p-2' > 
+                                <AccessTimeIcon/>
+                            </div> 
+                            <div className='p-2' > 
+                                Time &nbsp; 17:00
+                            </div>
+                        </div>
+                        <div className='d-flex row justify-content-start align-items-center'>
+                            <div className='p-2'> 
+                                <PlaceIcon/>
+                            </div> 
+                            <div className='p-2'> 
+                                Location &nbsp; 蒙民偉樓
+                            </div>
+                        </div>
+                        <div className='d-flex row justify-content-start align-items-center '>
+                            <div className='p-2'> 
+                                <PaymentIcon/>
+                            </div> 
+                            <div className='p-2'> 
+                                Ticket info. &nbsp; $0
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </Container>
-            
-            
+                <div className='d-flex row content justify-content-center'>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+                </div>
+            </div>
         );
     }
+}
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #f7a73e 30%, #f75a3e 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
+
+function Hook() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Save to Calendar</Button>;
 }
--- src/components/SearchPost.jsx
+++ src/components/SearchPost.jsx
@@ -19,6 +19,9 @@ import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import PlaceIcon from '@material-ui/icons/Place';
 import PaymentIcon from '@material-ui/icons/Payment';
 import EventIcon from '@material-ui/icons/Event';
+import LinesEllipsis from 'react-lines-ellipsis';
+import HTMLEllipsis from 'react-lines-ellipsis/lib/html'
+
 import './SearchPost.css'
 
 export default class SearchPost extends React.Component {
@@ -33,51 +36,61 @@ export default class SearchPost extends React.Component {
     }
     
     render() {
+        const text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,'
         return(       
             <div className='container'>
                 <div className='search-post d-flex row'> 
-                    <div className='poster col-4 '>
+                    <div className='poster col'>
                         <img src='./images/烤魷魚.jpg' className='img-fluid'/>
                     </div>
-                    <div className='content col-6 '>
-                        <div className=' title'>
+                    <div className='col'>
+                        <div className='content d-flex row justify-content-start align-items-center'>
+                        <div className='p-2 title'>
                             Welcome
                         </div>
-                        <div className='row'>
-                            <div className='col'>
-                                <div className='row'>
-                                    <EventIcon className='icon' fontSize='default' />
-                                    <div className='date'>
-                                        Date: 2020/05/22
-                                    </div>
-                                </div>
-                                <div className='row'>
-                                    <AccessTimeIcon className='icon' fontSize='default' />
-                                    <div className='time'>
-                                        Time: 17:00
-                                    </div>
-                                </div>
-                                <div className='row'>
-                                    <PlaceIcon className='icon' fontSize='default' />
-                                    <div className='location'>
-                                        Location: 野台
-                                    </div>
+                        <div className='p-2'>
+                            <div className='d-flex row justify-content-start align-items-center'>
+                                <div className='p-2'> 
+                                    <EventIcon/>
+                                </div> 
+                                <div className='p-2'>  
+                                    Date &nbsp; 2020/06/22
+                                </div> 
+                            </div>
+                            <div className= 'd-flex row justify-content-start align-items-center'>
+                                <div className='p-2' > 
+                                    <AccessTimeIcon/>
+                                </div> 
+                                <div className='p-2' > 
+                                    Time &nbsp; 17:00
                                 </div>
                             </div>
-                            <div className='row'>
-                                <div className='introduction'>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labinim veniam, quis nostrud e...       
+                            <div className='d-flex row justify-content-start align-items-center'>
+                                <div className='p-2'> 
+                                    <PlaceIcon/>
+                                </div> 
+                                <div className='p-2'> 
+                                    Location &nbsp; 蒙民偉樓
                                 </div>
                             </div>
+
+                            <div className='introduction'>
+                                    <HTMLEllipsis
+                                        unsafeHTML={text}
+                                        maxline={5}
+                                        basedOn='letters'
+                                    />
+                            </div>
+                            <div className='link'> 
+                                <Link to="/article" className="btn btn-secondary">
+                                    More Info
+                                </Link>
+                            </div>
+
                         </div>
-                        
-                        <div className='row link d-flex'> 
-                            <Link to="/article" className="btn btn-secondary">
-                                More Info
-                            </Link>
                         </div>
-                    
                     </div>
+                    
                 </div>
             </div>
         )
